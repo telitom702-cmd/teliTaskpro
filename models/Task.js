240a65e33fc09ff47154ac0b927a8fc62f337048
telitom702-cmd <telitom702@gmail.com>
2026-06-11 22:01:20 +0600

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,10 @@
-
+const mongoose = require('mongoose');
+const taskSchema = new mongoose.Schema({
+    title: String,
+    link: String,
+    reward: { type: Number, default: 0 },
+    isActive: { type: Boolean, default: true }, // রেওয়ার্ড অন/অফ
+    completedBy: [{ type: String }], // যারা টাস্ক করেছে
+    createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Task', taskSchema);
