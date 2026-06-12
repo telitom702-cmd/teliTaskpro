1bbffebf59138fb38bf7cb4ffd00754a30b4cf64
telitom702-cmd <telitom702@gmail.com>
2026-06-12 07:09:27 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,13 +1,1 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true }, // Telegram ID
-    username: String,
-    balance: { type: Number, default: 0 },
-    deviceIds: [String], // ডিভাইস আইডির লিস্ট
-    isBanned: { type: Boolean, default: false },
-    isVerified: { type: Boolean, default: false },
-    joinedAt: { type: Date, default: Date.now },
-    lastIP: String,
-    lastVPN: Boolean // VPN ব্যবহার করছে কিনা
-});
-module.exports = mongoose.model('User', userSchema);
+d
