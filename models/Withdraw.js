6c0f7d824fbccd2685a517485ef6ee7dc90329d2
telitom702-cmd <telitom702@gmail.com>
2026-06-12 07:09:55 +0600

    Update Withdraw.js

--- models/Withdraw.js
+++ models/Withdraw.js
@@ -1,10 +1,1 @@
-const mongoose = require('mongoose');
-const withdrawSchema = new mongoose.Schema({
-    userId: String,
-    method: { type: String, enum: ['bKash', 'Nagad', 'USD'] },
-    amount: Number,
-    accountNo: String,
-    status: { type: String, enum: ['Pending', 'Approved', 'Rejected'], default: 'Pending' },
-    requestedAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Withdraw', withdrawSchema);
+x
